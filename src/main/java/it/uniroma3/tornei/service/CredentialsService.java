1a51b62d877a3d3c738fab2ceaad4be866c68777
GioParz <giorgiasanetti04@gmail.com>
2026-06-30 20:22:19 +0200

    Inizio realizzazione del service

---
+++ src/main/java/it/uniroma3/tornei/service/CredentialsService.java
@@ -0,0 +1,38 @@
+package it.uniroma3.tornei.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import it.uniroma3.tornei.model.Credentials;
+import it.uniroma3.tornei.model.RuoloUtente;
+import it.uniroma3.tornei.repository.CredentialsRepository;
+
+@Service
+public class CredentialsService {
+	
+	@Autowired
+	private CredentialsRepository credentialsRepository;
+	
+	public Credentials getCredentials(Long id) {
+		
+		Optional<Credentials> result = this.credentialsRepository.findById(id);
+		
+		return result.orElse(null);
+	}
+	
+	public Credentials getcCredentials(String username) {
+		
+		Optional<Credentials> result = this.credentialsRepository.findByUsername(username);
+		
+		return result.orElse(null);
+	}
+	
+	public Credentials saveCredentials(Credentials credentials) {
+		
+		credentials.setRuolo(RuoloUtente.REGISTRATO);
+		
+		return this.credentialsRepository.save(credentials);
+	}
+}
